498ae1cc2f6259fc244d8772247a695abc9bfa77
Shubham-0004 <itsmeshubham2904@gmail.com>
2024-05-30 22:21:05 +0530

    Added new files

--- pattern printing/diamond.java
+++ pattern printing/diamond.java
@@ -3,6 +3,50 @@ import java.util.Scanner;
 public class diamond {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the order of diamond pattern--->");
+        int n=sc.nextInt();
+
+        //for the upper half part
+        for (int i = 1; i <=n; i++) {
+
+            // for the leading spaces
+            for (int j = n-i; j >=1; j--) {
+                System.out.print("   ");
+            }
+
+            // for the stars in middle
+            for (int j = 1; j <=(2*i)-1; j++) {
+                System.out.print(" * ");
+            }
+
+            // for the spaces at last
+            for (int j = n-i; j >=1; j--) {
+                System.out.print("   ");
+            }
+            System.out.println("\n");
+        }
+
+
+        // for the lower half part
+        for (int i = n; i >=1; i--) {
+
+            // for the leading spaces
+            for (int j = n-i; j >=1; j--) {
+                System.out.print("   ");
+            }
+
+            // for the stars in middle
+            for (int j = 1; j <=(2*i)-1; j++) {
+                System.out.print(" * ");
+            }
+
+            // for the spaces at last
+            for (int j = n-i; j >=1; j--) {
+                System.out.print("   ");
+            }
+            System.out.println("\n");
+        }
         sc.close();
     }
 }
